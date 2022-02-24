09c492e42dcc92f123699f4ba2e04e91eed0413c
Dean Attali <daattali@gmail.com>
2022-02-24 10:52:44 -0500

    plot: get rid of warnings for a cleaner console

--- R/plot.R
+++ R/plot.R
@@ -30,22 +30,22 @@ plot.ssvs <- function(x, y, MIP_threshold=0.5, ...){
   levels(plotDF$threshold) <- c(paste0('< ',MIP_threshold),paste0('>',MIP_threshold))
 
   plt <- ggplot2::ggplot(data=plotDF) +
-    ggplot2::geom_point(ggplot2::aes(x = reorder(plotDF$Variable_name,-plotDF$Inclusion_probability),
-                   y = plotDF$Inclusion_probability,
-                   shape = plotDF$threshold),
-               size = 2) +
-      ggplot2::labs(y = "Inclusion Probability",
-         x = "Predictor variables",
-         shape = "MIP threshold",
-         title = paste("Inclusion Probability for", y)) +
-      ggplot2::scale_y_continuous(limits = c(0,1.1), breaks = c(0, .25, .5, .75, 1)) +
-      ggplot2::theme_classic() +
-      ggplot2::geom_vline(xintercept = nrow(plotDF)+.5, linetype = 1, size = .5, alpha = .2) +
-      ggplot2::geom_hline(yintercept = MIP_threshold, linetype = 2) +
-      ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1),
-          panel.spacing = ggplot2::unit(0, "lines"),
-          strip.background = ggplot2::element_blank(),
-          strip.placement = "outside")
+    ggplot2::geom_point(ggplot2::aes(x = reorder(Variable_name,-Inclusion_probability),
+                                     y = Inclusion_probability,
+                                     shape = threshold),
+                        size = 2) +
+    ggplot2::labs(y = "Inclusion Probability",
+                  x = "Predictor variables",
+                  shape = "MIP threshold",
+                  title = paste("Inclusion Probability for", y)) +
+    ggplot2::scale_y_continuous(limits = c(0,1.1), breaks = c(0, .25, .5, .75, 1)) +
+    ggplot2::theme_classic() +
+    ggplot2::geom_vline(xintercept = nrow(plotDF)+.5, linetype = 1, size = .5, alpha = .2) +
+    ggplot2::geom_hline(yintercept = MIP_threshold, linetype = 2) +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1),
+                   panel.spacing = ggplot2::unit(0, "lines"),
+                   strip.background = ggplot2::element_blank(),
+                   strip.placement = "outside")
   plt
 }
 
