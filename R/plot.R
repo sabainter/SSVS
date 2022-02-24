0f093a46efda77101f4c4464ffb0aa1b0cde3870
Dean Attali <daattali@gmail.com>
2022-02-24 12:11:09 -0500

    plot: improve the threshold parameter to allow NULL

--- R/plot.R
+++ R/plot.R
@@ -2,7 +2,8 @@
 #'
 #' @param x An SSVS result object obtained from [`ssvs()`]
 #' @param y The name of the dependent variable
-#' @param MIP_threshold An MIP threshold to show on the plot
+#' @param threshold An MIP threshold to show on the plot, must be between 0-1.
+#' If `NULL`, no threshold is used.
 #' @param ... Ignored
 #' @examples
 #' outcome <- "qsec"
@@ -12,7 +13,7 @@
 #' @return Creates a plot of the inclusion probabilities by variable
 #' @export
 #' @importFrom rlang .data
-plot.ssvs <- function(x, y, MIP_threshold=0.5, ...){
+plot.ssvs <- function(x, y, threshold = 0.5, ...){
   assert_ssvs(x)
 
   #Recreate a dataframe of the results
@@ -22,27 +23,38 @@ plot.ssvs <- function(x, y, MIP_threshold=0.5, ...){
   names(plotDF) <- c("Inclusion_probability","Variable_name","Dependent_variable")
   plotDF <- plotDF[order(-plotDF$Inclusion_probability),]
 
-  plotDF$threshold <- ifelse(plotDF$Inclusion_probability>MIP_threshold, 1, 0)
-  plotDF$threshold <- as.factor(plotDF$threshold)
-  levels(plotDF$threshold) <- c(paste0('< ',MIP_threshold),paste0('>',MIP_threshold))
+  if (is.null(threshold)) {
+    plotDF$threshold <- as.factor(0)
+  } else {
+    plotDF$threshold <- ifelse(plotDF$Inclusion_probability > threshold, 1, 0)
+    plotDF$threshold <- as.factor(plotDF$threshold)
+    levels(plotDF$threshold) <- c(paste0('< ', threshold), paste0('> ', threshold))
+  }
 
-  plt <- ggplot2::ggplot(data=plotDF) +
+  plt <- ggplot2::ggplot(data = plotDF) +
     ggplot2::geom_point(ggplot2::aes(x = stats::reorder(.data[["Variable_name"]], -.data[["Inclusion_probability"]]),
                                      y = .data[["Inclusion_probability"]],
                                      shape = .data[["threshold"]]),
                         size = 2) +
     ggplot2::labs(y = "Inclusion Probability",
                   x = "Predictor variables",
-                  shape = "MIP threshold",
                   title = paste("Inclusion Probability for", y)) +
     ggplot2::scale_y_continuous(limits = c(0,1.1), breaks = c(0, .25, .5, .75, 1)) +
     ggplot2::theme_classic() +
     ggplot2::geom_vline(xintercept = nrow(plotDF)+.5, linetype = 1, size = .5, alpha = .2) +
-    ggplot2::geom_hline(yintercept = MIP_threshold, linetype = 2) +
     ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1),
                    panel.spacing = ggplot2::unit(0, "lines"),
                    strip.background = ggplot2::element_blank(),
                    strip.placement = "outside")
+
+  if (is.null(threshold)) {
+    plt <- plt + ggplot2::guides(shape = FALSE)
+  } else {
+    plt <- plt +
+      ggplot2::labs(shape = "MIP threshold") +
+      ggplot2::geom_hline(yintercept = threshold, linetype = 2)
+  }
+
   plt
 }
 
