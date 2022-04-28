7a1e72156bb4dc11c20dadc39c99a176073cef41
Dean Attali <daattali@gmail.com>
2022-04-28 17:09:48 -0400

    add 'color' param to plot()

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,6 @@
 # Unreleased version
 
+- Add a boolean `color` parameter to `plot()`
 - Don't automatically print the result of `summary()` to the console since it doesn't allow you to properly save the result to a variable
 
 # SSVS 1.0.0 (2022-03-08)
--- R/plot.R
+++ R/plot.R
@@ -6,6 +6,7 @@
 #' @param legend If `TRUE`, show a legend for the shapes based on the threshold.
 #' Ignored if `threshold = NULL`.
 #' @param title The title of the plot. Set to `NULL` to use a default title.
+#' @param color If `TRUE`, the data points will be colored based on the threshold.
 #' @param ... Ignored
 #' @examples
 #' \donttest{
@@ -17,7 +18,7 @@
 #' @return Creates a plot of the inclusion probabilities by variable
 #' @export
 #' @importFrom rlang .data
-plot.ssvs <- function(x, threshold = 0.5, legend = TRUE, title = NULL,
+plot.ssvs <- function(x, threshold = 0.5, legend = TRUE, title = NULL, color = TRUE,
                       ...) {
   assert_ssvs(x)
   checkmate::assert_number(threshold, lower = 0, upper = 1, null.ok = TRUE)
@@ -42,22 +43,30 @@ plot.ssvs <- function(x, threshold = 0.5, legend = TRUE, title = NULL,
     title <- paste("Inclusion Probability for", attr(x, "response"))
   }
 
+  if (color) {
+    cols <- c("#FF4D1C", "#225C3E")
+  } else {
+    cols <- c("black", "black")
+  }
+
   plt <- ggplot2::ggplot(data = plotDF) +
     ggplot2::geom_point(ggplot2::aes(x = stats::reorder(.data[["Variable_name"]], -.data[["Inclusion_probability"]]),
                                      y = .data[["Inclusion_probability"]],
-                                     shape = .data[["threshold"]]),
+                                     shape = .data[["threshold"]],
+                                     color = .data[["threshold"]]),
                         size = 2) +
     ggplot2::labs(y = "Inclusion Probability",
                   x = "Predictor variables",
                   title = title) +
     ggplot2::scale_y_continuous(limits = c(0,1.1), breaks = c(0, .25, .5, .75, 1)) +
+    ggplot2::scale_color_manual(values = cols) +
     ggplot2::theme_classic() +
     ggplot2::geom_vline(xintercept = nrow(plotDF)+.5, linetype = 1, size = .5, alpha = .2) +
     ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1),
                    panel.spacing = ggplot2::unit(0, "lines"),
                    strip.background = ggplot2::element_blank(),
                    strip.placement = "outside") +
-    ggplot2::guides(shape = FALSE)
+    ggplot2::guides(shape = FALSE, color = FALSE)
 
   if (!is.null(threshold)) {
     plt <- plt +
