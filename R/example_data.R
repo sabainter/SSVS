3b6076c86b7d3985f6c3df0fa9ac3ab49bc9c7f3
mzx <mzx@MacBook-Pro-146.local>
2024-12-15 00:51:35 -0500

    Add SSVS_MI function for multiple imputation analysis

--- R/example_data.R
+++ R/example_data.R
@@ -1,12 +1,12 @@
 #' Example Data for SSVS Analysis
 #'
-#' A dataset with 30 rows and 7 variables used for demonstrating the
+#' A dataset with 30 rows and 8 variables used for demonstrating the
 #' `SSVS_MI` function.
 #'
-#' @format A data frame with 30 rows and 7 variables:
+#' @format A data frame with 30 rows and 8 variables:
 #' \describe{
 #'   \item{.imp}{Imputation identifier (1 to 3).}
-#'   \item{r}{Replication identifier (1 to 10).}
+#'   \item{r}{Replication identifier (1 to 5).}
 #'   \item{yMCAR40}{Response variable.}
 #'   \item{xMCAR40_1}{Predictor variable 1.}
 #'   \item{xMCAR40_2}{Predictor variable 2.}
@@ -19,5 +19,5 @@
 #' data(example_data)
 #' outcome <- "yMCAR40"
 #' predictors <- c("xMCAR40_1", "xMCAR40_2", "xMCAR40_3", "xMCAR40_4", "xMCAR40_5")
-#' results <- SSVS_MI(data = example_data, y = outcome, x = predictors)
+#' results <- SSVS_MI(data = example_data, y = outcome, x = predictors, imputations = 3, replications = 5)
 "example_data"
