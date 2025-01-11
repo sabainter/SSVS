9ad58d7ba50b53264510564d83512300ceb6a20e
mzx <mzx@MacBook-Pro-146.local>
2025-01-11 17:43:49 -0500

    change plot functions

--- R/plot_MI.R
+++ R/plot_MI.R
@@ -17,53 +17,13 @@
 #' plot_ssvs_est(summary_stats, cond=FALSE)
 #' }
 #' @export
-plot_ssvs_est <- function(data, ty=NA, pal=NA, Condition=NA, cond=TRUE, title=NULL) {
-  checkmate::assert_data_frame(data, min.cols = 5)
-  checkmate::assert_character(ty, min.len = 1)
-  checkmate::assert_character(pal, len = length(ty))
+plot.est <- function(data, title=NULL) {
+  checkmate::assert_data_frame(data, min.cols = 4)
+  checkmate::assert_string(title, null.ok = TRUE)
 
   if (is.null(title)) {
     title <- "SSVS-MI estimates"
   }
-
-  if (cond) {
-    if (sum(is.na(ty))>0) {
-      stop("please input `ty`")
-    }
-    if (sum(is.na(pal))>0) {
-      stop("please input `pal`")
-    }
-    if (sum(is.na(Condition))>0) {
-      stop("please input `Condition`")
-    }
-    if (!all(unique(Condition) %in% ty)) {
-      stop("Levels in `Condition` do not match `ty`. Please ensure they align.")
-    }
-  }
-
-  if (cond) {
-
-  data$Condition = Condition
-  data$Condition = factor(data$Condition, levels=ty)
-
-  plt <- ggplot2::ggplot(data, ggplot2::aes(x = forcats::fct_inorder(Variables), y = avg.beta)) +
-    ggplot2::geom_errorbar(
-      ggplot2::aes(ymin = min, ymax = max, colour = Condition),
-      position = "dodge"
-    ) +
-    ggplot2::scale_color_manual("Condition", values = setNames(pal, ty)) +
-    ggplot2::geom_point(
-      ggplot2::aes(fill = Condition, shape = Condition, colour = Condition),
-      position = ggplot2::position_dodge(0.9)
-    ) +
-    ggplot2::scale_fill_manual("Condition", values = setNames(pal, ty)) +
-    ggplot2::ggtitle(title) +
-    ggplot2::xlab("Variables") +
-    ggplot2::ylab("Mean Coefficients") +
-    ggplot2::theme_bw() +
-    ggplot2::theme_classic()
-
-  } else {
 
   plt <- ggplot2::ggplot(data, ggplot2::aes(x = forcats::fct_inorder(Variables), y = avg.beta)) +
       ggplot2::geom_errorbar(
@@ -77,7 +37,77 @@ plot_ssvs_est <- function(data, ty=NA, pal=NA, Condition=NA, cond=TRUE, title=NU
         y = "Mean Coefficients"
       ) +
       ggplot2::theme_classic()
+
+  plt
+}
+
+plot.mip <- function(data, threshold = 0.5, legend = TRUE, title = NULL, color = TRUE) {
+  checkmate::assert_data_frame(data, min.cols = 4)
+  checkmate::assert_number(threshold, lower = 0, upper = 1, null.ok = TRUE)
+  checkmate::assert_logical(legend, len = 1, any.missing = FALSE)
+  checkmate::assert_string(title, null.ok = TRUE)
+
+  plotDF <- data[order(-data$avg.mip),]
+
+  if (is.null(threshold)) {
+    plotDF$threshold <- as.factor(0)
+  } else {
+    plotDF$threshold <- ifelse(plotDF$avg.mip > threshold, 1, 0)
+    plotDF$threshold <- factor(plotDF$threshold, levels = c(0, 1))
+    levels(plotDF$threshold) <- c(paste0('< ', threshold), paste0('> ', threshold))
+  }
+
+  if (is.null(title)) {
+    title <- "Multiple Inclusion Probability for SSVS-MI"
+  }
+
+  if (color) {
+    cols <- c("#FF4D1C", "#225C3E")
+  } else {
+    cols <- c("black", "black")
+  }
+
+  plt <- ggplot2::ggplot(data = plotDF) +
+    ggplot2::geom_point(ggplot2::aes(x = stats::reorder(.data[["Variables"]], -.data[["avg.mip"]]),
+                                     y = .data[["avg.mip"]],
+                                     shape = .data[["threshold"]],
+                                     color = .data[["threshold"]]),
+                        size = 2) +
+    ggplot2::geom_errorbar(aes(x = .data[["Variables"]],
+                               y = .data[["avg.mip"]],
+                               ymin = .data[["min"]],
+                               ymax = .data[["max"]]),
+                           width = .10,
+                           position = position_dodge(0.05)) +
+    ggplot2::labs(y = "Multiple Inclusion Probability",
+                  x = "Predictor variables",
+                  title = title) +
+    ggplot2::scale_y_continuous(limits = c(0,1.1), breaks = c(0, .25, .5, .75, 1)) +
+    ggplot2::scale_color_manual(values = cols) +
+    ggplot2::theme_classic() +
+    ggplot2::geom_vline(xintercept = nrow(plotDF)+.5, linetype = 1, size = .5, alpha = .2) +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1),
+                   panel.spacing = ggplot2::unit(0, "lines"),
+                   strip.background = ggplot2::element_blank(),
+                   strip.placement = "outside")
+
+  if (!is.null(threshold)) {
+    plt <- plt +
+      ggplot2::labs(shape = "MIP threshold", color = "MIP threshold") +
+      ggplot2::geom_hline(yintercept = threshold, linetype = 2)
+    if (legend) {
+      plt <- plt + ggplot2::guides(shape = "legend", color = "legend")
+    } else {
+      plt <- plt + ggplot2::guides(shape = "none", color = "none")  # Correctly disable the legends using "none"
+    }
   }
 
   plt
 }
+
+
+
+
+
+
+
