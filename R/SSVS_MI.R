8212890bf1a420bd1037c22ff1d12438f45b76bf
mzx <mzx@MacBook-Pro-146.local>
2025-01-10 22:25:31 -0500

    Change SSVS_MI function and add imputed_mtcars data

--- R/SSVS_MI.R
+++ R/SSVS_MI.R
@@ -4,58 +4,52 @@
 #' for a given set of predictors and a response variable. It supports continuous response variables and calculates
 #' aggregated results across multiple imputations and replications.
 #'
-#' @param data A dataframe containing the variables of interest, including an `.imp` column for imputation identifiers
-#'   and an `r` column for replication identifiers.
+#' @param data A dataframe containing the variables of interest, including an `.imp` column for imputation identifiers.
 #' @param y The response variable (character string).
 #' @param x A vector of predictor variable names.
-#' @param imputations The number of imputations to process (default is 25).
-#' @param replications The number of replications per imputation (default is 500).
+#' @param imp The imputation variable.
+#' @param imputations The number of imputations to process (default is 5).
 #' @param interval Confidence interval level for summary results (default is 0.9).
 #' @param continuous Logical indicating if the response variable is continuous (default is TRUE).
 #' @param progress Logical indicating whether to display progress (default is FALSE).
 #'
-#' @return An SSVS object containing aggregated results across imputations and replications, including mean
+#' @return An SSVS object containing aggregated results across imputations, including mean
 #'   inclusion probabilities and average beta coefficients for each predictor.
 #' @examples
 #' \donttest{
-#' data(example_data)
-#' outcome <- "yMCAR40"
-#' predictors <- c("xMCAR40_1", "xMCAR40_2", "xMCAR40_3", "xMCAR40_4", "xMCAR40_5")
-#' results <- SSVS_MI(data = example_data, y = outcome, x = predictors, imputations = 3, replications = 3)
+#' data(imputed_mtcars)
+#' outcome <- 'qsec'
+#' predictors <- c('cyl', 'disp', 'hp', 'drat', 'wt', 'vs', 'am', 'gear', 'carb','mpg')
+#' imputation <- '.imp'
+#' results <- SSVS_MI(data = imputed_mtcars, y = outcome, x = predictors, imp = imputation)
 #' }
 #' @export
-SSVS_MI <- function(data, y, x, imputations = 25, replications = 500,
+ssvs_mi <- function(data, y, x, imp, imputations = 5,
                          interval = 0.9, continuous = TRUE, progress = FALSE) {
   checkmate::assert_data_frame(data, min.rows = 1)
   checkmate::assert_character(y, len = 1)
   checkmate::assert_character(x, min.len = 1)
+  checkmate::assert_character(imp, min.len = 1)
   checkmate::assert_logical(continuous, len = 1)
   checkmate::assert_logical(progress, len = 1)
 
   final_results <- data.frame(Variables = x)
-  all_data <- NULL
 
   for (i in 1:imputations) {
-    final_results = data.frame(Variables = x)
-    for (r in 1:replications) {
-      temp <- data[data$.imp == i & data$r == r, c(x, y)]
+      data <- data %>%
+        dplyr::filter(imp==i)
+      temp <- data[, c(x, y)]
       results <- SSVS::ssvs(data = temp, x = x, y = y, continuous = continuous, progress = progress)
       summary_results <- summary(results, interval = interval, ordered = FALSE)
-      final_results = merge(final_results, summary_results[, c('MIP', 'Avg Beta', 'Avg Nonzero Beta')], by = 0, all = TRUE, sort = FALSE) [-1]
+      final_results <- merge(final_results, summary_results[, c('MIP', 'Avg Beta', 'Avg Nonzero Beta')], by = 0, all = TRUE, sort = FALSE) [-1]
       names(final_results)[names(final_results) == "MIP"] <- paste(r, "MIP")
       names(final_results)[names(final_results) == "Avg Beta"] <- paste(r, "Avg Beta")
       names(final_results)[names(final_results) == 'Avg Nonzero Beta'] <- paste(r,'Avg Nonzero Beta')
-    }
-    all_data <- rbind(all_data, final_results)
   }
 
-  avg_imp <- all_data %>%
-    dplyr::group_by(Variables) %>%
-    dplyr::summarise_all("mean")
+  class(final_results) <- c("ssvs", class(final_results))
+  attr(final_results, "response") <- y
 
-  class(avg_imp) <- c("ssvs", class(avg_imp))
-  attr(avg_imp, "response") <- y
-
-  avg_imp
+  final_results
 }
 
